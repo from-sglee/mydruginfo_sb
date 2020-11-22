36cba903d78669316a53fecfde8f3da0e5164d40
from-sglee <from.sglee@gmail.com>
2020-11-23 02:06:25 +0900

    add character setting for UTF-8

--- src/main/java/com/mydruginfo/MydruginfoApplication.java
+++ src/main/java/com/mydruginfo/MydruginfoApplication.java
@@ -2,7 +2,21 @@ package com.mydruginfo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
+import javax.servlet.Filter;
+import java.nio.charset.Charset;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 @SpringBootApplication
 public class MydruginfoApplication {
 
@@ -10,4 +24,16 @@ public class MydruginfoApplication {
 		SpringApplication.run(MydruginfoApplication.class, args);
 	}
 
+    @Bean
+    public HttpMessageConverter<String> responseBodyConverter() {
+        return new StringHttpMessageConverter(Charset.forName("UTF-8"));
+    }
+
+    @Bean
+    public Filter characterEncodingFilter() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return characterEncodingFilter;
+    }
 }
