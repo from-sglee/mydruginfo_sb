f27631e5377fea80bacc641deb4cbad4f100cd54
DESKTOP-8HMNF74\Myungjun David <audwns7456@gmail.com>
2020-11-30 11:36:03 +0900

    admin페이지 insurance_covered, main_ingredient 버튼

--- src/main/java/com/mydruginfo/AdminController.java
+++ src/main/java/com/mydruginfo/AdminController.java
@@ -1,8 +1,8 @@
 package com.mydruginfo;
 
-
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,50 +11,104 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.mydruginfo.model.InsuranceCovered;
+import com.mydruginfo.model.MainIngredient;
 import com.mydruginfo.model.StandardCode;
 import com.mydruginfo.paging.Pagination;
 import com.mydruginfo.service.AdminService;
 
 @Controller
 public class AdminController {
-	
+
 	@Autowired
 	AdminService service;
-	
+
 	@RequestMapping("/adminpage")
-	public ModelAndView adminpage(@RequestParam(defaultValue="1") int page, HttpSession session) {
-		
-		
+	public ModelAndView adminpage(@RequestParam(defaultValue = "1") int page, HttpSession session,
+			HttpServletRequest request) {
+
 		ModelAndView modelAndView = new ModelAndView();
-		
-		if(session.getAttribute("admin_id") == null) {
-			
+
+		if (session.getAttribute("admin_id") == null) {
+
 			modelAndView.setViewName("page/login");
-			
+
 			return modelAndView;
 		}
-		
+
 		else {
-			
+
+			String button_value = null;
+
 			int totalListCnt = service.findAllCnt();
-			
+
 			Pagination pagination = new Pagination(totalListCnt, page);
-			
+
 			int startIndex = pagination.getStartIndex();
 			int pageSize = pagination.getPageSize();
-			
+
 			List<StandardCode> list = service.AllStandardCode(startIndex, pageSize);
-			
-			
+			button_value = "standard_code";
 			modelAndView.addObject("List", list);
 			modelAndView.addObject("pagination", pagination);
+			modelAndView.addObject("button_value", button_value);
 			modelAndView.setViewName("page/adminpage");
-			
+
 			return modelAndView;
-			
+
 		}
-		
-		
+
+	}
+
+	@RequestMapping("/adminpage_insurance_covered")
+	public ModelAndView adminpageIC(@RequestParam(defaultValue = "1") int page, HttpSession session,
+			HttpServletRequest request) {
+
+		ModelAndView modelAndView = new ModelAndView();
+
+		String button_value = null;
+		int totalListCnt = service.findAllCnt();
+
+		Pagination pagination = new Pagination(totalListCnt, page);
+
+		int startIndex = pagination.getStartIndex();
+		int pageSize = pagination.getPageSize();
+
+		List<InsuranceCovered> list = service.AllInsuranceCovered(startIndex, pageSize);
+		button_value = "insurance_covered";
+		modelAndView.addObject("List", list);
+		modelAndView.addObject("button_value", button_value);
+		modelAndView.addObject("pagination", pagination);
+		modelAndView.setViewName("page/adminpage");
+
+		return modelAndView;
+
+	}
+
+	@RequestMapping("/adminpage_main_ingredient")
+	public ModelAndView adminpageMI(@RequestParam(defaultValue = "1") int page, HttpSession session,
+			HttpServletRequest request) {
+
+		ModelAndView modelAndView = new ModelAndView();
+
+		String button_value = null;
+
+		int totalListCnt = service.findAllCnt();
+
+		Pagination pagination = new Pagination(totalListCnt, page);
+
+		int startIndex = pagination.getStartIndex();
+		int pageSize = pagination.getPageSize();
+
+		List<MainIngredient> list = service.AllMainIngredient(startIndex, pageSize);
+		button_value = "main_ingredient";
+		modelAndView.addObject("List", list);
+		modelAndView.addObject("pagination", pagination);
+		modelAndView.addObject("button_value", button_value);
+		modelAndView.setViewName("page/adminpage");
+
+		return modelAndView;
+
 	}
 
 }
--- src/main/java/com/mydruginfo/repository/AdminMapper.java
+++ src/main/java/com/mydruginfo/repository/AdminMapper.java
@@ -1,6 +1,8 @@
 package com.mydruginfo.repository;
 
 import com.mydruginfo.model.AdminVO;
+import com.mydruginfo.model.InsuranceCovered;
+import com.mydruginfo.model.MainIngredient;
 import com.mydruginfo.model.StandardCode;
 
 import org.apache.ibatis.annotations.Mapper;
@@ -14,5 +16,7 @@ public interface AdminMapper {
     List<AdminVO> updateAdmin(AdminVO vo);
     int findAllCnt();
     List<StandardCode> AllStandardCode(int startIndex,int pageSize);
+    List<InsuranceCovered> AllInsuranceCovered(int startIndex,int pageSize);
+    List<MainIngredient> AllMainIngredient(int startIndex,int pageSize);
   
 }
