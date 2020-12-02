c01b98e391d1ce3994d37deb168ba9e66edfae96
from-sglee <from.sglee@gmail.com>
2020-12-02 15:09:32 +0900

    update  insu controller

--- src/main/java/com/mydruginfo/InsuranceCoveredController.java
+++ src/main/java/com/mydruginfo/InsuranceCoveredController.java
@@ -5,8 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
